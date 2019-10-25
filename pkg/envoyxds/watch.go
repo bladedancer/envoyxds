bfb422001e85c6ade23e8e1faf09688a2bf31fa8
Gavin Matthews <gmatthews@axway.com>
2019-10-25 18:33:24 +0100

    Not working yet.

--- pkg/envoyxds/watch.go
+++ pkg/envoyxds/watch.go
@@ -11,12 +11,17 @@ var version int = 0
 
 //Load file and add a snapshot, incrementing version each time
 func loadSnapshot(numTenants int, numRoutes int, domain string, c cache.SnapshotCache) {
+	listenerConfig := GetListener(0, domain)
+	lrs := []cache.Resource{
+		listenerConfig,
+	}
+
 	routeConfig := GetRouteConfigurations(numTenants, numRoutes, domain)
 	rts := []cache.Resource{
 		routeConfig,
 	}
 	log.Infof("Num Tenants: %d, Num Routes %d, Domain: %s", numTenants, numRoutes, domain)
-	err := c.SetSnapshot("shard-0", cache.NewSnapshot(fmt.Sprintf("%d", version), nil, nil, rts, nil))
+	err := c.SetSnapshot("shard-0", cache.NewSnapshot(fmt.Sprintf("%d", version), nil, nil, rts, lrs))
 	version++
 	if err != nil {
 		log.Error(err)
@@ -36,6 +41,11 @@ func watch(snapshotCache cache.SnapshotCache, conf Config) {
 func pump(snapshotCache cache.SnapshotCache, conf Config) {
 	tick := time.NewTicker(time.Duration(conf.Pump) * time.Second)
 	i := 1
+
+	// listenerConfig := GetListener(0, conf.Domain)
+	// lrs := []cache.Resource{
+	// 	listenerConfig,
+	// }
 
 	go func() {
 		for {
